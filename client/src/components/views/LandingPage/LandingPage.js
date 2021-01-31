2c56c3f0f2e7bab635b36b85da26643466ac6b07
User <njs1324@gmail.com>
2021-01-31 21:14:13 +0900

    20210131 commit

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function LandingPage() {
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(res => {console.log(res)})
+    }, [])
+
     return (
-        <div>
-            LandingPage
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'
+                    , width: '100%' , height: '100vh'}
+                    }>
+            <h2>시작페이지</h2>
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
@@ -27,6 +27,8 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+app.get('/api/hello', (req,res) => res.send('Hello World!~~~'))
 
 
 app.post('/api/users/register', (req,res) => {
